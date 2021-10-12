2a317c5841296e9447fe1505fb02794982cba1bb
HIROPONFILMS <zm19291@josai.ac.jp>
2021-10-12 02:06:25 +0000

    2001.10.12

---
+++ myjs.js
@@ -0,0 +1,13 @@
+alert('Hello, Hiroki!');
+// lert('Hello, Ando!');
+// alert(1+1);
+// alert(Math.sqrt(1+1));
+// alert(Math.sqrt(2+3));
+// alert(Math.sqrt(1/0));
+// alert(Math.sqrt(0/0));
+
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
