885bf84fcf90b758c4031d4bac708b5b22fed7fd
HIROPONFILMS <zm19291@josai.ac.jp>
2021-11-30 03:50:54 +0000

    1130

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,2 @@
-function imgchange1() {document.getElementById('logo'). src= './image/keiei17.jpg' ;}
+function imgchange1(){document.getElementById('logo').src='./image/keiei17.jpg';}
+function imgchange2(){document.getElementById('logo').src='./image/keiei17.jpg';}
